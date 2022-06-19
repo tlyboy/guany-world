ccd995bc3bdc0df6d96e1d5030be67d797a40ae0
Guany <1084072690@qq.com>
2022-06-19 14:01:16 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('header')
-  const start = document.querySelector('.start')
-  const messages = document.querySelectorAll('.message')
+const header = document.querySelector('header')
+const start = document.querySelector('.start')
+const messages = document.querySelectorAll('.message')
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > header.offsetHeight) {
-      header.style.backdropFilter = 'blur(10px)'
-      header.style.webkitBackdropFilter = 'blur(10px)'
+window.addEventListener('scroll', () => {
+  if (window.scrollY > header.offsetHeight) {
+    header.style.backdropFilter = 'blur(10px)'
+    header.style.webkitBackdropFilter = 'blur(10px)'
+  } else {
+    header.style.backdropFilter = 'none'
+    header.style.webkitBackdropFilter = 'none'
+  }
+
+  for (let i = 0; i < messages.length; i++) {
+    const img = messages[i].querySelector('img')
+    if (window.scrollY > img.offsetTop - start.offsetHeight) {
+      img.classList.add('layui-anim', 'layui-anim-scale')
     } else {
-      header.style.backdropFilter = 'none'
-      header.style.webkitBackdropFilter = 'none'
+      img.classList.remove('layui-anim', 'layui-anim-scale')
     }
+  }
+})
 
-    for (let i = 0; i < messages.length; i++) {
-      const img = messages[i].querySelector('img')
-      if (window.scrollY > img.offsetTop - start.offsetHeight) {
-        img.classList.add('layui-anim', 'layui-anim-scale')
-      } else {
-        img.classList.remove('layui-anim', 'layui-anim-scale')
-      }
-    }
-  })
+const joinGame = document.querySelector("a[href='#download']")
+const download = document.querySelector('#download')
 
-  const joinGame = document.querySelector("a[href='#download']")
-  const download = document.querySelector('#download')
+joinGame.addEventListener('click', e => {
+  if (window.scrollTo) {
+    e.preventDefault()
+    window.scrollTo({ behavior: 'smooth', top: download.offsetTop })
+  }
+})
 
-  joinGame.addEventListener('click', e => {
-    if (window.scrollTo) {
-      e.preventDefault()
-      window.scrollTo({ behavior: 'smooth', top: download.offsetTop })
-    }
-  })
-
-  download.addEventListener('click', () => {
-    layer.open({
-      title: '消息：',
-      content: '正在整合中……'
-    })
+download.addEventListener('click', () => {
+  layer.open({
+    title: '消息：',
+    content: '正在整合中……'
   })
 })
